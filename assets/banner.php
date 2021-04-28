fa3f33680655909db7e567077f5da18973270cfd
Bryan DELAITRE Q5NSQAM <bryan.delaitre.study@gmail.com>
2021-04-28 23:32:21 +0200

    v0.1.1.5' debug'

---
+++ assets/banner.php
@@ -0,0 +1,17 @@
+<script type="text/javascript" src="assets/banner.js"></script>
+<div class="banner_Carousel_slider" id="left"></div>
+<div class="banner_Carousel_slider" id="right"></div>
+<section class="banner">
+    <div id="1">
+        <img src>
+        <h4></h4>
+    </div>
+    <div id="two">
+        <img>
+        <h4></h4>
+    </div>
+    <div id="3">
+        <img>
+        <h4></h4>
+    </div>
+</section>
