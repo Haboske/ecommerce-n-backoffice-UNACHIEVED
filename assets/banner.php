cc870cab4227b55d27cc9fe26d7cab6187880873
Bryan DELAITRE <bryan.delaitre.study@gmail.com>
2021-04-28 21:22:59 +0200

    v0.0.1.4

---
+++ assets/banner.php
@@ -0,0 +1,14 @@
+<section class="banner">
+    <div id="1">
+        <img src>
+        <h4></h4>
+    </div>
+    <div id="2">
+        <img>
+        <h4></h4>
+    </div>
+    <div id="3">
+        <img>
+        <h4></h4>
+    </div>
+</section>
---
+++ assets/header.php
@@ -0,0 +1,79 @@
+<header>
+    <img src="svg/logo_1.svg">
+    <nav>
+        <a href="#" class="link_market_t">Boutique
+            <div id="link_market"></div>
+        </a>
+        <a href="#" class="link_account_t">Se connecter
+            <div id="link_account"></div>
+        </a>
+        <a href="#" class="link_cart_t">Mon Panier
+            <div id="link_cart"></div>
+        </a>
+    </nav>
+    <form>
+        <label for="vinyl-search">Recherchez un vinyle</label>
+        <input type="search" id="vinyl-search" name="vinyl-search" placeholder="rechercher par titre&#47;artiste&#47;album...">
+        <button type="submit" id=test>
+            <div></div>
+        </button>
+    </form>
+    <nav id="jqnav" class="navigation">
+      
+      <input type="button" id="leave">
+      <span class="link_category_type">
+        <h4>Tous les produits </h4><span>&gt;</span>
+      </span>
+      <span class="link_category">
+        <a href='#'>top ventes</a>
+      </span>
+      <span class="link_category">
+        <a href='#'>nouveaut&eacute;s</a>
+      </span>
+      <span class="link_category">
+        <a href='#'>offres limit&eacute;s</a>
+      </span>
+      <span class="link_category">
+        <a href='#'>occasions</a>
+      </span>
+      <span class="link_category">
+        <a href='#test'>mat&eacute;riels&#47;accessoires</a>
+      </span>
+      <span class="link_category_type">
+        <h4>cat&eacute;gories</h4>
+      </span>
+      <span class="link_category">
+        <a href='#'>pop&#47;rock&#47;m&eacute;tal</a>
+      </span>
+      <span class="link_category">
+        <a href='#'>vari&eacute;t&eacute;s françaises</a>
+      </span>
+      <span class="link_category">
+        <a href='#'>rap</a>
+      </span>
+      <span class="link_category">
+        <a href='#'>musiques de films</a>
+      </span>
+      <span class="link_category">
+        <a href='#'>raggae&#47;raggaeton</a>
+      </span>
+      <span class="link_category">
+        <a href='#'>classique</a>
+      </span>
+      <span class="link_category">
+        <a href='#'>country</a>
+      </span>
+      <span class="link_category">
+        <a href='#'>electro</a>
+      </span>
+      <span class="link_category">
+        <a href='#'>jazz</a>
+      </span>
+      <span class="link_category">
+        <a href='#'>sixties-rock'n'roll</a>
+      </span>
+      
+    </nav>
+      <div class="right"></div>
+</header>
+
